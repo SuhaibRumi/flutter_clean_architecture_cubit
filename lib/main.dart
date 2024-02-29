2e37245c0ffdeaa9cb60403fd5ebeaa1f3ddbf3f
SuhaibAppsol <suhaib@appsol360.com>
2024-03-01 00:06:51 +0500

    removed_ unused file

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Cubit',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const UserListPage(),
     );
